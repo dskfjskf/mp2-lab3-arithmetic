7536e5d191e35e46265a959bee07230a56586e09
dskfjskf <32364547+dskfjskf@users.noreply.github.com>
2017-11-09 11:31:21 +0300

    a

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,88 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+
+#include <iostream>
+
+using namespace std;
+
+template <class ValType>
+class Stack
+{
+protected:
+	ValType *ps;					//Указатель на эл-ты стека
+	int Size;						// Mаксимальное количество элементов в стеке
+	int top;						// Номер текущего элемента стека
+public:
+	Stack(int s = 5);							// Конструктор 
+	~Stack();									// Деструктор
+	void push(const ValType&v);					// Вставка элемента
+	ValType pop();								// Извлечение элемента
+	ValType peek() const;						// Просмотр верхнего элемента (без удаления)
+	bool empty() const;							// Проверка на пустоту
+	int GetIndex() const { return top + 1; }	// Получение количества элементов в стеке
+	void clears() { top = -1; }					// Очистка стека
+
+};
+
+template <class ValType>						// Конструктор 
+Stack <ValType>::Stack(int s)
+{
+	Size = s;	
+	top = -1;
+	ps = new ValType[Size];
+}
+
+template <class ValType>						// Деструктор
+Stack<ValType>::~Stack()
+{
+	delete[]ps;
+}
+
+template<class ValType>							// Вставка элемента
+void Stack<ValType>::push(const ValType& v)
+{
+	if (top == (Size - 1))
+	{
+		Size *= 2;
+		ValType *ps1 = new ValType[Size];
+		for (int i = 0; i < (top + 1); i++)
+			ps1[i] = ps[i];		
+		delete[] ps;
+		ps = ps1;
+	}
+	ps[++top] = v;
+}
+
+template<class ValType>							// Извлечение элемента
+ValType Stack<ValType>::pop()
+{
+	if (top!=-1)
+	return ps[top--];
+	else throw " ";
+}
+
+
+template<class ValType>							// Просмотр верхнего элемента (без удаления)
+ValType Stack<ValType>::peek() const
+{ 
+	if (top!=-1)
+	return ps[top];
+	else throw " ";
+}
+
+template<class ValType>							// Проверка на пустоту
+bool Stack<ValType>::empty() const
+{
+	return top==-1
+}
+
+
+
+
+
+
+
+
+
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,4 +1,47 @@
-// тесты для стека
-
 #include "stack.h"
 #include <gtest.h>
+
+TEST(Stack, can_create_stack)
+{
+	ASSERT_NO_THROW(Stack <int> a(4));
+	ASSERT_NO_THROW(Stack <int> a);
+}
+
+TEST(Stack, can_push_value)
+{
+	Stack <int> a(2);
+	ASSERT_NO_THROW(a.push(3));
+}
+
+TEST(Stack, can_push)
+{
+	Stack <int> a(1);
+	a.push(2);
+	ASSERT_NO_THROW(a.push(3));
+}
+
+TEST(Stack, can_pop_value)
+{
+	Stack <int> a;
+	a.push(5);
+	EXPECT_EQ(5,a.pop());
+}
+
+TEST(Stack, can_pop)
+{
+	Stack <int> a;
+	ASSERT_ANY_THROW(a.pop());
+}
+
+TEST(Stack, can_peek_value)
+{
+	Stack <int> a;
+	a.push(5);
+	EXPECT_EQ(5, a.peek());
+}
+
+TEST(Stack, can_peek)
+{
+	Stack <int> a;
+	ASSERT_ANY_THROW(a.peek());
+}
