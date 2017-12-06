45bad60abaa8ae656384fa2fa61a41f4d32a0089
dskfjskf <32364547+dskfjskf@users.noreply.github.com>
2017-12-06 23:27:16 +0300

    привет

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -5,7 +5,11 @@
 const string allOperators = "+-*/()";
 
 enum lexemtype { LBRACKET, RBRACKET, OPERATOR, VALUE, UNKNOWN }; 
-struct lexem { string str; lexemtype lt; };
+struct lexem 
+{
+	string str; 
+	lexemtype lt;
+};
 
 class arithmetic
 {		
@@ -13,15 +17,17 @@ class arithmetic
 	lexem* pL; //Массив лексем	
 	string inputStr; //Выражение
 	void parse();//Разбор на лексемы
-	
-	
-public:	
-	bool Check_B();
-	bool Check_S();
+	bool Check_B();//Проверка на скобки
+	bool Check_S();//Проверка на символы
+	bool Check_С();//Проверка на корректность
+	bool PR(lexem a, lexem b);//Приоритет
+	int RPN(lexem* pLex);//Перевод в ОПЗ
+public:		
 	bool Check();
+	double Cal();//Вычислениие
 	arithmetic(const string& istr);
 	~arithmetic() { delete[] pL; }
-
+	
 };
 
 void arithmetic::parse()
@@ -133,11 +139,11 @@ bool arithmetic::Check_B()
 	return res;
 }
 
-bool arithmetic::Check()
+bool arithmetic::Check_С()
 {
 	bool res = true;
 	int pos = -1;
-	if (!(pL[0].lt == VALUE || pL[0].lt == LBRACKET))
+	if (!(pL[0].lt == VALUE || pL[0].lt == LBRACKET || pL[0].str[0]=='-'))
 	{
 		res = false;
 		cout << "No operand before 1" <<  endl;
@@ -150,15 +156,130 @@ bool arithmetic::Check()
 			cout << "No operator between:" << pos + 1 << "and" << pos + 2 << endl;
 			res = false;
 		}
-		if ((pL[i].lt == LBRACKET || pL[i].lt == OPERATOR) && (pL[i + 1].lt == OPERATOR || pL[i + 1].lt == RBRACKET))
+		if (pL[i].lt == LBRACKET && ((pL[i + 1].lt == OPERATOR && pL[i+1].str[0]!='-') || pL[i + 1].lt == RBRACKET))
 		{
 			cout << "No operand between:" << pos + 1 << "and" << pos + 2 << endl;
 			res = false;
-		}				
+		}			
+		if (pL[i].lt == OPERATOR && (pL[i + 1].lt == OPERATOR || pL[i + 1].lt == RBRACKET))
+		{
+			cout << "No operand between:" << pos + 1 << "and" << pos + 2 << endl;
+			res = false;
+		}
 	}
 	return res;
 }
 
+bool arithmetic::Check() 
+{
+	if (Check_S())
+	return Check_B()&&Check_С();
+	else 	
+		return false;
+}
+
+bool arithmetic::PR(lexem a, lexem b)
+{
+	int p1, p2;
+	switch (a.str[0])
+	{
+	case '(':p1=0;
+		break;
+	case '+':;
+	case '-':p1 = 1;
+		break;
+	default:p1 = 2;		
+	}
+
+	switch (b.str[0])
+	{
+	case '(':p2 = 0;
+		break;
+	case '+':;
+	case '-':p2 = 1;
+		break;
+	default:p2 = 2;
+	}
+
+	return p1 > p2;
+}
+
+int arithmetic::RPN(lexem* pLex)
+{
+	lexem zero;
+	zero.lt = VALUE;
+	zero.str = "0";
+
+	lexemtype cur;	
+	int j = 0;
+	Stack<lexem> s(nL);
+	for (int i = 0; i < nL; i++)
+	{
+		cur = pL[i].lt;
+		if (cur == VALUE)
+		{
+			pLex[j] = pL[i];
+			j++;
+		}
+		if (cur == LBRACKET)
+			s.push(pL[i]);
+		if (cur == RBRACKET)
+		{
+			while ((s.peek()).lt != LBRACKET)
+			{
+				pLex[j] = s.pop();
+				j++;
+			}
+			s.pop();
+		}
+		if (cur == OPERATOR)
+		{
+			if (pL[i].str[0] == '-' && (i == 0 || pL[i - 1].str[0] == '(' ))
+			{
+				pLex[j] = zero;
+				j++;
+			}
+			if (!s.empty() && !PR(pL[i], s.peek()))
+			{
+				pLex[j] = s.pop();
+				j++;
+			}
+			s.push(pL[i]);
+		}
+
+		}
+	while (!s.empty())
+	{
+		pLex[j] = s.pop();
+		j++;
+	}
+	return j;
+}
+
+double arithmetic::Cal()
+{
+	lexem* pLex = new lexem[2*nL];
+	int fnL = RPN(pLex);
+	Stack<double> res(2*fnL);
+	for (int i = 0; i < fnL; i++)
+		if (pLex[i].lt == VALUE)
+			res.push(stod(pLex[i].str));
+		else 
+		{
+			double r = res.pop(), l = res.pop();
+			switch (pLex[i].str[0])
+			{
+			case '+': res.push(l + r);
+				break;
+			case '-': res.push(l - r);
+				break;
+			case '*': res.push(l * r);
+				break;
+			default: res.push(l / r);				
+			}
+		}
+	return res.peek();
+}
 
 arithmetic::arithmetic(const string& istr)
 	{
--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,8 +1,94 @@
 #include "arithmetic.h"
 #include <gtest.h>
 
-TEST(a, b)
+#define EPS 10e-15
+
+struct check_test
 {
-	arithmetic a("5(5");
-	a.Check();
+	string str;
+	bool res;
+	check_test(string istr, bool ires)
+	{
+		str = istr;
+		res = ires;
+
+	}
+};
+
+struct cal_test
+{
+	string str;
+	double res;
+	cal_test(string istr, double ires)
+	{
+		str = istr;
+		res = ires;
+	}
+};
+
+class a_check : public ::testing::TestWithParam<check_test>
+{
+protected: arithmetic a_test;
+public:
+	a_check() : a_test(GetParam().str)
+	{
+	}
+	~a_check() {}
+};
+
+class a_cal : public ::testing::TestWithParam<cal_test>
+{
+protected: arithmetic b_test;
+istringstream input;
+public:
+	a_cal() : b_test(GetParam().str)
+	{
+	}
+	~a_cal() {}
+};
+
+TEST_P(a_check, can_check_for_correct)
+{
+	EXPECT_EQ(GetParam().res, a_test.Check());
 }
+
+TEST_P(a_cal, can_calculate_expressions)
+{
+	EXPECT_GE(EPS, abs(GetParam().res - b_test.Cal()));
+}
+
+INSTANTIATE_TEST_CASE_P(Inst1,
+	a_check,
+	::testing::Values(					
+		check_test("5%", false),	
+		check_test("     ", false),
+		check_test("5+.5", false),
+		check_test("5+3.", false),
+		check_test("2.9.8", false),
+		check_test("((2)", false),
+		check_test("(2))", false),
+		check_test("1)(", false),
+		check_test("+3", false),		
+		check_test("5 5", false),
+		check_test("2(1)", false),
+		check_test("(2)1", false),
+		check_test("8++1", false),
+		check_test("()", false),
+		check_test("(+2)", false),
+		check_test("(1+)", false),
+		check_test("(-1)", true),
+		check_test("-1", true),
+		check_test("3+(-1*8)", true)
+	));
+
+INSTANTIATE_TEST_CASE_P(Inst2,
+	a_cal,
+	::testing::Values(
+		cal_test("5+5", 10.0),
+		cal_test("25+3*(7-2)*2-3",52.0 ),
+		cal_test("-2*8.1",-16.2 ),
+		cal_test("6*(-5)/3", -10.0),
+		cal_test("9*0+7/(5+2)*8",8.0 ),
+		cal_test("(8.1+9.9)/2",9.0 )		
+	));
+
